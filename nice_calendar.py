94ce029f2d294e1f03baa276a1e3ef38e42ebdc2
Karloid <krldboyan@gmail.com>
2018-09-08 18:11:01 +0300

    - new pattern

--- nice_calendar.py
+++ nice_calendar.py
@@ -4,24 +4,29 @@ import datetime
 print("Starting... hello people ")
 print(str(datetime.datetime.today()))
 
-fileName = "state"
+def getCommitsCount(weekday):
+    currentDay = weekday
+    normalizedDay = currentDay + 1
+    if currentDay == 6:
+        normalizedDay = 0
+    return abs(normalizedDay - 3) + 1
 
-currentDay = datetime.datetime.today().weekday()
-
-commitsCount = currentDay + 2
-if currentDay == 6:
-    commitsCount = 1
+commitsCount = getCommitsCount(datetime.datetime.today().weekday())
 
 print("commitsCount " + str(commitsCount))
 
-for i in range(commitsCount):
-    print("do commit")
 
-    os.system("git pull")
+def doGitStuff():
+    for i in range(commitsCount):
+        print("do commit")
 
-    with open(fileName, 'a') as file:
-        file.write("\ni:" + str(i) + " " + str(datetime.datetime.today()))
+        os.system("git pull")
 
-    os.system("git add . && git commit -m \"update calendar\" && git push -u origin")
+        with open("state", 'a') as file:
+            file.write("\ni:" + str(i) + " " + str(datetime.datetime.today()))
+
+        os.system("git add . && git commit -m \"update calendar\" && git push -u origin")
+
+doGitStuff()
 
 print("finished, I hope that everything is ok")
